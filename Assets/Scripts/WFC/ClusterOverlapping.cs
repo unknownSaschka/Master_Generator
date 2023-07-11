7639e8c9d840068630a43456b37417703e1f87f1
unknownSaschka <schuetzel24@freenet.de>
2023-07-11 17:41:20 +0200

    plan for next day

--- Assets/Scripts/WFC/ClusterOverlapping.cs
+++ Assets/Scripts/WFC/ClusterOverlapping.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClusterOverlapping : MonoBehaviour
+public class ClusterOverlapping : Model
 {
-    // Start is called before the first frame update
-    void Start()
+    Dictionary<string, List<byte[]>> patterns;      //Node or Clustername to Patterns
+    List<int> colors;
+
+    public ClusterOverlapping(Dictionary<string, Node> nodes, Texture2D clusterMap, int N, int width, int height, bool periodicInput, bool periodic, int symmetry, bool ground, Heuristic heuristic)
+        : base(width, height, N, periodic, heuristic)
     {
-        
+        Dictionary<string, byte[]> samples = new();
+
+        //load all samples for each node
+
+        //get all colors from all samples
+
+        //get all patterns and save them for each node (needs all colors to calculate hashes)
+
+        //calculate proper weights from all patterns
+
+        //create propagator with clusterMap in mind
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public override void Save(string filename)
     {
-        
+        throw new System.NotImplementedException();
     }
+
+    static byte[] pattern(Func<int, int, byte> f, int N)
+    {
+        byte[] result = new byte[N * N];
+        for (int y = 0; y < N; y++) for (int x = 0; x < N; x++) result[x + y * N] = f(x, y);
+        return result;
+    }
+    static byte[] rotate(byte[] p, int N) => pattern((x, y) => p[N - 1 - y + x * N], N);
+    static byte[] reflect(byte[] p, int N) => pattern((x, y) => p[N - 1 - x + y * N], N);
 }
